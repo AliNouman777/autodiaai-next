f6b5aad65d87113110565d3922bff88425a75dee
AliNouman777 <hb6348248@gmail.com>
2025-08-27 21:09:00 +0500

    update base url

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
-// const BASE_URL = "https://api.autodia.tech";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://api.autodia.tech";
+// const BASE_URL = "http://localhost:4000";
 
 export class ApiError extends Error {
   status?: number;
