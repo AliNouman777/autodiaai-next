82b03b08440d3c68f1f2330280ae6bbbb588579a
AliNouman007 <alinouman6348@gmail.com>
2025-09-03 10:12:23 +0500

    update the url

--- BaseUrl.ts
+++ BaseUrl.ts
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://api.autodia.tech";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://api.autodia.tech";
+// const BASE_URL = "http://localhost:4000";
 
 export default BASE_URL;
