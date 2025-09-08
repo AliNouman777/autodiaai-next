7db116f0be389749171a1d74b81e0b8ce661961b
AliNouman007 <alinouman6348@gmail.com>
2025-09-08 14:39:19 +0500

    add the new blog

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
+import * as blog_1 from "../blog/content/database-schema-from-natural-language.mdx?collection=blog&hash=1757064711958"
 import * as blog_0 from "../blog/content/ai-erd-generator.mdx?collection=blog&hash=1757064711958"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const blog = _runtime.docs<typeof _source.blog>([{ info: {"path":"ai-erd-generator.mdx","absolutePath":"/home/inspireuplift/Downloads/audodiaai-next/blog/content/ai-erd-generator.mdx"}, data: blog_0 }], [])
+export const blog = _runtime.docs<typeof _source.blog>([{ info: {"path":"ai-erd-generator.mdx","absolutePath":"/home/inspireuplift/Downloads/audodiaai-next/blog/content/ai-erd-generator.mdx"}, data: blog_0 }, { info: {"path":"database-schema-from-natural-language.mdx","absolutePath":"/home/inspireuplift/Downloads/audodiaai-next/blog/content/database-schema-from-natural-language.mdx"}, data: blog_1 }], [])
